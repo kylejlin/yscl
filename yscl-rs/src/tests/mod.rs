60dd1acab8ff0230ebbc9ad6aed41eb3ed3a2741
Kyle Lin <kylejlin.us@gmail.com>
2023-01-04 00:59:20 -0800

    Remove newline

--- yscl-rs/src/tests/mod.rs
+++ yscl-rs/src/tests/mod.rs
@@ -9,7 +9,6 @@ use assert::*;
 #[test]
 fn hello_world() {
     let src = include_str!("sample_code/correct/hello_world.yscl");
-
     let expected = map! {
         "kantu_version" = atom!("1.0.0"),
         "dependencies" = map! {
