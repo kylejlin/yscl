a969a39b42f9568f4a3a8b78396d07323d6280e6
Kyle Lin <kylejlin.us@gmail.com>
2023-01-04 01:19:42 -0800

    Add parse! macro

--- yscl-rs/src/tests/macros.rs
+++ yscl-rs/src/tests/macros.rs
@@ -1,26 +1,45 @@
 macro_rules! atom {
-    ($value:literal) => {
+    ($value:literal) => {{
+        use $crate::*;
         Node::Atom(Atom {
             value: $value.to_string(),
         })
-    };
+    }};
 }
 
 macro_rules! map {
-    ($($key:literal = $value:expr),*) => {
+    ($($key:ident = $value:expr),*) => {{
+        use $crate::*;
         Node::Map(Map {
             entries: vec![$(MapEntry {
-                key: Identifier::new($key.to_string()).expect("Invalid identifier name"),
+                key: Identifier::new(stringify!($key).to_string()).expect("Invalid identifier name"),
                 value: $value,
             }),*],
         })
-    };
+    }};
 }
 
 macro_rules! list {
-    ($($element:expr),*) => {
+    ($($element:expr),*) => {{
+        use $crate::*;
         Node::List(List {
             elements: vec![$($element),*],
         })
+    }};
+}
+
+macro_rules! parse {
+    ($value:literal) => {
+        atom!($value)
+    };
+    ({$($key:ident = $value:tt),*}) => {
+        map! {
+            $($key = parse!($value)),*
+        }
+    };
+    ([$($element:tt),*]) => {
+        list! {
+            $(parse!($element)),*
+        }
     };
 }
--- yscl-rs/src/tests/mod.rs
+++ yscl-rs/src/tests/mod.rs
@@ -9,28 +9,28 @@ use assert::*;
 #[test]
 fn hello_world() {
     let src = include_str!("sample_code/correct/hello_world.yscl");
-    let expected = map! {
-        "kantu_version" = atom!("1.0.0"),
-        "dependencies" = map! {
-            "foo" = atom!("2.0.3"),
-            "bar" = atom!("bar"),
-            "lorem" = map! {
-                "url" = atom!("https://github.com/kylejlin/nonexistent_repo")
+    let expected = parse! ({
+        kantu_version = "1.0.0",
+        dependencies = {
+            foo = "2.0.3",
+            bar = "bar",
+            lorem = {
+                url = "https://github.com/kylejlin/nonexistent_repo"
             }
         },
-        "licenses" = list! [
-            atom!("MIT"),
-            atom!("APACHE"),
-            map! {
-                "url" = atom!("https://github.com/kylejlin/nonexistent_repo/CUSTOM_LICENSE")
+        licenses = [
+            "MIT",
+            "APACHE",
+            {
+                url = "https://github.com/kylejlin/nonexistent_repo/CUSTOM_LICENSE"
             }
         ],
-        "sequences" = list! [
-            atom!("\""),
-            atom!("\\"),
-            atom!("\n"),
-            atom!("\u{263A}")
+        sequences = [
+            "\"",
+            "\\",
+            "\n",
+            "\u{263A}"
         ]
-    };
+    });
     expected_success(src, &expected);
 }
